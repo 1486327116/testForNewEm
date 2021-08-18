1d09a1bbad977e3179a6c10ab84474149847260c
1486327116 <1486327116@qq.com>
2021-08-18 14:06:39 +0800

    1

---
+++ src/main/java/com/example/testfornewem/TestfornewemApplication.java
@@ -0,0 +1,13 @@
+package com.example.testfornewem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestfornewemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestfornewemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testfornewem/TestfornewemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testfornewem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestfornewemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
